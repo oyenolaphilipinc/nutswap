29e416d2871d0c8cfe5e0b8797e9f6ba30493273
draysongz <dharmiegra4@gmail.com>
2024-06-03 02:34:40 +0100

    done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,17 @@ import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
+
+const manifestUrl =
+  "https://raw.githubusercontent.com/Draysongz/nutswap/main/public/manifest.json";
 export default function App({ Component, pageProps }) {
   return (
+    <>
+     <TonConnectUIProvider manifestUrl={manifestUrl}>
     <ChakraProvider>
     <Component {...pageProps} />
     </ChakraProvider>
+    </TonConnectUIProvider>
+    </>
   );
 }
