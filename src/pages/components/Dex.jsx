fad5b1b54fe215a38fcffe1f2fe4ae696ea026c1
draysongz <dharmiegra4@gmail.com>
2024-06-05 00:07:10 +0100

    dd

--- src/pages/components/Dex.jsx
+++ src/pages/components/Dex.jsx
@@ -897,7 +897,7 @@ async function sendFee(amount) {
 
           <Button alignSelf={'center'} w={useBreakpointValue({ base: "80%", medium: "80%", lg: "27vw" })}
            mb={4} bgColor={'#FFFF6C'} h={'8vh'} borderRadius={'10px'} _hover={{ bg: "#FFFF6C", opacity: 0.8 }} onClick={handleSwap} 
-           >{connected ? 'SWAP' : 'CONNECT WALLET'}</Button>
+           >{ 'SWAP' }</Button>
         </Flex>
 
         <Flex
