922d59a94a5b045ae66fe79febb496c272f8a1c6
draysongz <dharmiegra4@gmail.com>
2024-06-05 00:13:21 +0100

    dd

--- src/Hooks/useTonConnect.ts
+++ src/Hooks/useTonConnect.ts
@@ -1,33 +1,44 @@
 import { useTonConnectUI, useTonAddress } from "@tonconnect/ui-react";
-
 import { Address, Sender, SenderArguments } from "@ton/core";
+import { useState, useEffect } from 'react';
 
 export function useTonConnect(): {
-  sender: Sender;
+  sender: Sender | null;
   connected: boolean;
-  userAddress: string;
+  userAddress: string | null;
 } {
   const [tonConnectUI] = useTonConnectUI();
   const TONAddress = useTonAddress(true);
-  return {
-    sender: {
-      send: async (args: SenderArguments) => {
-        await tonConnectUI.sendTransaction({
-          messages: [
-            {
-              address: args.to.toString(),
-              amount: args.value.toString(),
-              payload: args.body?.toBoc().toString("base64"),
-            },
-          ],
-          validUntil: Date.now() + 5 * 60 * 1000, // 5 minutes for user to approve
-        });
-      },
-      address: TONAddress ? Address.parse(TONAddress) : undefined
-    },
 
-    connected: tonConnectUI.connected,
-    userAddress: TONAddress,
-    
+  const [connected, setConnected] = useState(false);
+  const [userAddress, setUserAddress] = useState<string | null>(null);
+  const [sender, setSender] = useState<Sender | null>(null);
+
+  useEffect(() => {
+    if (tonConnectUI && TONAddress) {
+      setConnected(tonConnectUI.connected);
+      setUserAddress(TONAddress);
+      setSender({
+        send: async (args: SenderArguments) => {
+          await tonConnectUI.sendTransaction({
+            messages: [
+              {
+                address: args.to.toString(),
+                amount: args.value.toString(),
+                payload: args.body?.toBoc().toString("base64"),
+              },
+            ],
+            validUntil: Date.now() + 5 * 60 * 1000, // 5 minutes for user to approve
+          });
+        },
+        address: Address.parse(TONAddress),
+      });
+    }
+  }, [tonConnectUI, TONAddress]);
+
+  return {
+    sender,
+    connected,
+    userAddress,
   };
 }
